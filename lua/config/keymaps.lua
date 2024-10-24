625112b45641594ee0f33d6499160e9b393f4c57
howard0615 <howard159632@gmail.com>
2024-10-24 19:33:13 +0800

    new config

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,19 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+-- Add any additional keymaps here
+
+
+local keymaps = vim.keymap
+local opts = { normap = true, silent = true }
+ 
+
+-- Increment/decrement
+keymap.set("n", "+", "<C-a>")
+keymap.set("n", "-", "<C-x>")
+
+-- Delete a word backwards
+keymap.set("n", "dw", 'vb"_d')
+
+-- Select all 
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
