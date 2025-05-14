d0495ad5f789ac0ad8f375ffc87007d02aa4cd0f
howard0615 <howard159632@gmail.com>
2025-05-15 00:06:04 +0800

    Change color theme into onedark

--- lua/howardcheng/lazy/color.lua
+++ lua/howardcheng/lazy/color.lua
@@ -1,9 +1,17 @@
+-- function ColorMyPencils(color)
+-- 	color = color or "rose-pine-moon"
+-- 	vim.cmd.colorscheme(color)
+
+-- 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+-- end
+
 function ColorMyPencils(color)
-	color = color or "rose-pine-moon"
+	color = color or "onedark"
 	vim.cmd.colorscheme(color)
 
-	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
-	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+	-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
 end
 
 return {
@@ -12,12 +20,20 @@ return {
         "erikbackman/brightburn.vim",
     },
 
+    -- {
+    --     "folke/tokyonight.nvim",
+    --     lazy = false,
+    --     opts = {},
+    --     config = function()
+    --         ColorMyPencils()
+    --     end
+    -- },
     {
-        "folke/tokyonight.nvim",
-        lazy = false,
-        opts = {},
-        config = function()
-            ColorMyPencils()
+        "olimorris/onedarkpro.nvim",
+        config = function ()
+            require("onedarkpro").setup({
+                ColorMyPencils()
+            })
         end
     },
     {
