5710b848ef3f0246274bc89df216afc2cd136b6a
howard0615 <howard159632@gmail.com>
2025-05-15 00:05:17 +0800

    Optimize set.lua - Adding shell setting for terminal command - Enable searching highlight - Adding fold method

--- lua/howardcheng/set.lua
+++ lua/howardcheng/set.lua
@@ -12,13 +12,19 @@ vim.opt.smartindent = true
 
 vim.opt.wrap = false
 
+vim.opt.foldmethod = "expr"
+vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+vim.opt.foldlevel = 99 -- don't fold initial
+
+
 vim.opt.swapfile = false
 vim.opt.backup = false
 vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
-vim.opt.hlsearch = false
+vim.opt.hlsearch = true
 vim.opt.incsearch = true
+vim.opt.ignorecase = true
 
 vim.opt.termguicolors = true
 
@@ -28,4 +34,13 @@ vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
 
-vim.opt.colorcolumn = "80"
+-- powershell setting to avoid strange text output using :!
+vim.opt.shell = vim.fn.executable "pwsh" and "pwsh" or "powershell"
+vim.opt.shellcmdflag = "-NoLogo -NoProfile -ExecutionPolicy RemoteSigned -Command [Console]::InputEncoding=[Console]::OutputEncoding=[System.Text.Encoding]::UTF8;$PSStyle.OutputRendering = [System.Management.Automation.OutputRendering]::PlainText;"
+vim.opt.shellredir = "-RedirectStandardOutput %s -NoNewWindow -Wait"
+vim.opt.shellpipe = "2>&1 | Out-File -Encoding UTF8 %s; exit $LastExitCode"
+vim.opt.shellquote = ""
+vim.opt.shellxquote = ""
+
+
+-- vim.opt.colorcolumn = "80"
